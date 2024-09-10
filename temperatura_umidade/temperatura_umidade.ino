f6f35ae2da3146e2348767f1da9463840e8ca7a6
felipe-lim4 <felipe.vieira@sptech.school>
2024-09-10 12:14:25 -0300

    Atualização de codigo arduino

--- temperatura_umidade/temperatura_umidade.ino
+++ temperatura_umidade/temperatura_umidade.ino
@@ -13,9 +13,8 @@ const int PINO_SENSOR_DHT11 = A1;
 DHT sensorDHT(PINO_SENSOR_DHT11, TIPO_SENSOR);
 
 // CRIANDO UMA CONSTANTE INTEIRA CHAMADA PINO_SENSOR_TEMPERATURA E DEFININDO A PORTA A0 PARA ELA
-// CRIANDO UMA VARIÁVEL QUE ACEITA NÚMERO DECIMAL CHAMADA temperaturaCelsius
 const int PINO_SENSOR_TEMPERATURA = A0;
-float temperaturaCelsius;
+
 
 // FUNÇÃO QUE INICIA AS PORTAS // FUNÇÃO QUE LIGA O SENSOR E MANDA CAPTURAR COM A FUNÇÃO sensorDHT
 void setup() {
@@ -27,7 +26,7 @@ void setup() {
 // REALIZA A CONVERSÃO DO valorLeitura em TemperaturaCelsius através de cálculos matemáticos
 void loop() {
   int valorLeitura = analogRead(PINO_SENSOR_TEMPERATURA);
-  temperaturaCelsius = (valorLeitura * 5.0/1023.0) / 0.01;
+  float temperaturaCelsius = (valorLeitura * 5.0/1023.0) / 0.01;
 
   float umidade = sensorDHT.readHumidity();
   
@@ -37,12 +36,18 @@ void loop() {
     
     //PASSE PELA VALIDAÇÃO, ENTRA NO PRINT DOS RESULTADOS
   } else{
-    Serial.println(umidade);
+    // REALIZA PRINTS de forma seriada do texto "Temperatura: (valor da variável)C" a cada 1000ms
+    Serial.print("Umidade:");
+    Serial.print(umidade);
+    Serial.print(' ');
+    Serial.print("TempMaximo:");
+    Serial.print(40);
+    Serial.print(' ');
+    Serial.print("Temperatura:");
+    Serial.print(temperaturaCelsius);
+    Serial.print(' ');
+    Serial.print("TempMínimo:");
+    Serial.println(20);
+    delay(1000);
   }
-
-// REALIZA PRINTS de forma seriada do texto "Temperatura: (valor da variável)C" a cada 2000ms
- 
-  Serial.println(temperaturaCelsius);
- 
-  delay(1000);
 }
